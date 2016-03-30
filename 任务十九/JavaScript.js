36d385568f4e3c125ed5eb3b2629f1ccec5cba9a
JX-Zhuang <411546535@qq.com>
2016-03-30 23:20:37 +0800

    add

--- 任务十九/JavaScript.js
+++ 任务十九/JavaScript.js
@@ -26,7 +26,7 @@ window.onload = function() {
         leftAdd: function(value) {
             if (this.searchValue(value) == true) {
                 value = Number(value);
-                if(data.length==60) return (alert('不能大于60个'));
+                if (data.length == 60) return (alert('不能大于60个'));
                 data.unshift(value);
                 render();
                 console.log(data)
@@ -35,7 +35,7 @@ window.onload = function() {
         rightAdd: function(value) {
             if (this.searchValue(value) == true) {
                 value = Number(value);
-                if(data.length==60) return (alert('不能大于60个'));
+                if (data.length == 60) return (alert('不能大于60个'));
                 data.push(value);
                 render();
             }
@@ -51,78 +51,84 @@ window.onload = function() {
 
         }, //右侧出
         sortRender: function() {
-            var col = document.querySelectorAll('#aqi-div div');
-            var block = document.querySelectorAll('#aqi-num span');
-            var i = col.length,
-                j = 0;
-            var num,
-                text;
-            var a;
-            function forJ() {
-                if (Number(col[j].innerHTML) > Number(col[j + 1].innerHTML)) {
-                    //柱状图
-                    col[j].style.backgroundColor='red';
-
-                    text = col[j + 1].innerHTML;
-                    col[j + 1].innerHTML = col[j].innerHTML;
-                    col[j].innerHTML = text;
-                    num = col[j + 1].style.cssText;
-                    col[j + 1].style.cssText = col[j].style.cssText;
-                    col[j].style.cssText = num;
-                    //文字
-                    text = block[j + 1].innerHTML;
-                    block[j + 1].innerHTML = block[j].innerHTML;
-                    block[j].innerHTML = text;
-                    //style
-                    num = block[j + 1].style.cssText;
-                    block[j + 1].style.cssText = block[j].style.cssText;
-                    block[j].style.cssText = num;
-                    
-                    num = data[j + 1];
-                    data[j + 1] = data[j];
-                    data[j] = num;
-                }else{
-                    col[j].style.backgroundColor=col[j].style.backgroundColor;
-                }
-                j++;
-                if (j < i - 1) {
-                    var interval = setInterval(function() {
-                        a=1;
-                        forJ();
-                        clearInterval(interval);
-                    }, 300);
-                } else {
+                var col = document.querySelectorAll('#aqi-div div');
+                var block = document.querySelectorAll('#aqi-num span');
+                var i = col.length,
                     j = 0;
-                    i--;
-                    if (i == 0) {
-                        return true;
+                var num,
+                    text;
+                var a;
+                var color;
+                function forJ() {
+                    if (a!=i) {
+                        color=col[j].style.backgroundColor;
+                    }
+                    if (Number(col[j].innerHTML) > Number(col[j + 1].innerHTML)) {
+                        col[j].style.backgroundColor = 'red';
+                        //柱状图
+                        text = col[j + 1].innerHTML;
+                        col[j + 1].innerHTML = col[j].innerHTML;
+                        col[j].innerHTML = text;
+                        num = col[j + 1].style.cssText;
+                        col[j + 1].style.cssText = col[j].style.cssText;
+                        col[j].style.cssText = num;
+                        //文字
+                        text = block[j + 1].innerHTML;
+                        block[j + 1].innerHTML = block[j].innerHTML;
+                        block[j].innerHTML = text;
+                        //style
+                        num = block[j + 1].style.cssText;
+                        block[j + 1].style.cssText = block[j].style.cssText;
+                        block[j].style.cssText = num;
+
+                        num = data[j + 1];
+                        data[j + 1] = data[j];
+                        data[j] = num;
                     } else {
+                        col[j].style.backgroundColor = color;
+                        color = col[j + 1].style.backgroundColor;
+                        col[j + 1].style.backgroundColor = 'red';
+                    }
+                    a=i;
+                    j++;
+                    if (j < i - 1) {
                         var interval = setInterval(function() {
                             forJ();
                             clearInterval(interval);
                         }, 300);
+                    } else {
+                        col[j].style.backgroundColor = color;
+                        j = 0;
+                        i--;
+                        if (i == 0) {
+                            return true;
+                        } else {
+                            var interval = setInterval(function() {
+                                forJ();
+                                clearInterval(interval);
+                            }, 300);
+                        }
                     }
+                    return data;
                 }
-                return data;
-            }
-            forJ();
-        }//,
-        // sort: function(arr) {
-        //         var i = arr.length,
-        //             j;
-        //         var num;
-        //         for (i; i > 0; i--) {
-        //             for (j = 0; j < i - 1; j++) {
-        //                 //arr[j]=Number(arr[j]);
-        //                 if (arr[j] > arr[j + 1]) {
-        //                     num = arr[j + 1];
-        //                     arr[j + 1] = arr[j];
-        //                     arr[j] = num;
-        //                 }
-        //             }
-        //         }
-        //         return arr;
-        //     } //数组排序
+                forJ();
+            } //,
+            // sort: function(arr) {
+            //         var i = arr.length,
+            //             j;
+            //         var num;
+            //         for (i; i > 0; i--) {
+            //             for (j = 0; j < i - 1; j++) {
+            //                 //arr[j]=Number(arr[j]);
+            //                 if (arr[j] > arr[j + 1]) {
+            //                     num = arr[j + 1];
+            //                     arr[j + 1] = arr[j];
+            //                     arr[j] = num;
+            //                 }
+            //             }
+            //         }
+            //         return arr;
+            //     } //数组排序
     };
     /**
      *绑定事件
